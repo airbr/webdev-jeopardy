e0dff040d872667fda632cb3bf092e0903619d23
B. Morgan Murrah <beau@airbridge.ac.nz>
2016-08-02 22:18:35 -0400

    Error identified with use of break

--- js/main.js
+++ js/main.js
@@ -181,28 +181,26 @@ function submit() {
           console.log("You selected the correct answer:" + questions[i].correctAnswer);
           var scs = i;
           console.log("The current question in index is: " + scs);
-
-              if ( scs === 0 || scs === 5 || scs === 10 || scs === 15 || scs === 20 ) {
-                score1 = score1 + 10;
-                console.log(score1);
-              } else if ( scs === 1 || scs === 6 || scs === 11 || scs === 16 || scs === 21 ) {
-                score1 = score1 + 50;
-                console.log(score1);
-              } else if ( scs === 2 || scs === 7 || scs === 12 || scs === 17 || scs === 22 ) {
-                score1 = score1 + 100;
-                console.log(score1);
-              } else if ( scs === 3 || scs === 8 || scs === 13 || scs === 18 || scs === 23 ) {
-                score1 = score1 + 500;
-                console.log(score1);
-              } else if ( scs === 4 || scs === 9 || scs === 14 || scs === 19 || scs === 24 ) {
-                score1 = score1 + 1000;
-                console.log(score1);
-              }
+        }
+        if ( scs === 0 || scs === 5 || scs === 10 || scs === 15 || scs === 20 ) {
+            score1 = score1 + 10;
+            console.log(score1);
+          } else if ( scs === 1 || scs === 6 || scs === 11 || scs === 16 || scs === 21 ) {
+            score1 = score1 + 50;
+            console.log(score1);
+          } else if ( scs === 2 || scs === 7 || scs === 12 || scs === 17 || scs === 22 ) {
+            score1 = score1 + 100;
+            console.log(score1);
+          } else if ( scs === 3 || scs === 8 || scs === 13 || scs === 18 || scs === 23 ) {
+            score1 = score1 + 500;
+            console.log(score1);
+          } else if ( scs === 4 || scs === 9 || scs === 14 || scs === 19 || scs === 24 ) {
+            score1 = score1 + 1000;
+            console.log(score1);
           }
       for (i = 0; i < questions.length; i++) {
       if ( selected !== questions[i].correctAnswer ) {
          console.log( selected + " was the wrong answer");
-         break;
          }
       }
   }
