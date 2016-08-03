7ac8a167496d7f60adf2986d53902e69ee2ab20c
B. Morgan Murrah <beau@airbridge.ac.nz>
2016-08-03 00:23:22 -0400

    Fixed, preparing for adding in Score add function again

--- js/main.js
+++ js/main.js
@@ -1,25 +1,5 @@
 console.log('JS Testing.');
 
-// // Making Answer buttons only clickable once
-// $('.btn-outline-secondary').click(function(){
-//    $(this).prop('disabled', true);
-// });
-// $('.correct').click(function(){
-//   $(alert("Correct!"));
-//   $('.correct').toggleClass('successanswer');
-//   $(score1).append('$10');
-// });
-// $('.incorrect').click(function(){
-//   $(alert("Nope."));
-//   $(score2).append('$-10');
-// });
-
-
-// // Player change Animation
-// $( "playertag" ).click(function() {
-//   $( this ).toggleClass( ".highlight" );
-// });
-
 
 /* Cred to Dr Mike VVV */
 var questions = [
@@ -175,36 +155,44 @@ $(function() {
 });
 function submit() {
   var selected = $(".modal-body input:checked").val();
-      for (i = 0; i < questions.length; i++) {
+    for (i = 0; i < questions.length; i++) {
         if ( selected === questions[i].correctAnswer ) {
           console.log(selected + " was selected");
           console.log("You selected the correct answer:" + questions[i].correctAnswer);
           var scs = i;
-          console.log("The current question in index is: " + scs);
         }
-        if ( scs === 0 || scs === 5 || scs === 10 || scs === 15 || scs === 20 ) {
-            score1 = score1 + 10;
-            console.log(score1);
-          } else if ( scs === 1 || scs === 6 || scs === 11 || scs === 16 || scs === 21 ) {
-            score1 = score1 + 50;
-            console.log(score1);
-          } else if ( scs === 2 || scs === 7 || scs === 12 || scs === 17 || scs === 22 ) {
-            score1 = score1 + 100;
-            console.log(score1);
-          } else if ( scs === 3 || scs === 8 || scs === 13 || scs === 18 || scs === 23 ) {
-            score1 = score1 + 500;
-            console.log(score1);
-          } else if ( scs === 4 || scs === 9 || scs === 14 || scs === 19 || scs === 24 ) {
-            score1 = score1 + 1000;
-            console.log(score1);
-          }
-      for (i = 0; i < questions.length; i++) {
-      if ( selected !== questions[i].correctAnswer ) {
-         console.log( selected + " was the wrong answer");
-         }
-      }
-  }
+          else if ( selected !== questions[i].correctAnswer ) {
+            console.log("You chose the wrong answer.");
+            return;
+        }
+    }
 }
+
+
+//         if ( scs === 0 || scs === 5 || scs === 10 || scs === 15 || scs === 20 ) {
+//             score1 = score1 + 10;
+//             console.log(score1);
+//           } else if ( scs === 1 || scs === 6 || scs === 11 || scs === 16 || scs === 21 ) {
+//             score1 = score1 + 50;
+//             console.log(score1);
+//           } else if ( scs === 2 || scs === 7 || scs === 12 || scs === 17 || scs === 22 ) {
+//             score1 = score1 + 100;
+//             console.log(score1);
+//           } else if ( scs === 3 || scs === 8 || scs === 13 || scs === 18 || scs === 23 ) {
+//             score1 = score1 + 500;
+//             console.log(score1);
+//           } else if ( scs === 4 || scs === 9 || scs === 14 || scs === 19 || scs === 24 ) {
+//             score1 = score1 + 1000;
+//             console.log(score1);
+//           }
+//       console.log(selected + "= selected");
+//       for (i = 0; i < questions.length; i++) {
+//       if ( selected !== questions[i].correctAnswer ) {
+//          console.log( selected + "was the wrong answer");
+//          }
+//       }
+//   }
+// }
 function getOptions(question) {
   var $buttonDiv = $('<div class="btn-group" data-toggle="buttons"></div>');
   question.options.forEach(function(opt) {
@@ -245,6 +233,26 @@ $("#myModal").on('hidden.bs.modal', function () {
 
 
 
+
+// // Making Answer buttons only clickable once
+// $('.btn-outline-secondary').click(function(){
+//    $(this).prop('disabled', true);
+// });
+// $('.correct').click(function(){
+//   $(alert("Correct!"));
+//   $('.correct').toggleClass('successanswer');
+//   $(score1).append('$10');
+// });
+// $('.incorrect').click(function(){
+//   $(alert("Nope."));
+//   $(score2).append('$-10');
+// });
+
+
+// // Player change Animation
+// $( "playertag" ).click(function() {
+//   $( this ).toggleClass( ".highlight" );
+// });
 
 
 
